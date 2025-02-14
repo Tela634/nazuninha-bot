cc0cdc3fb8e291bd6ac36796ad7487d3f5648c95
hiudyy <hiudyyy@gmail.com>
2025-02-14 11:56:11 -0300

    atualizando...

--- dados/src/.funcs/.downloads/.youtube.js
+++ dados/src/.funcs/.downloads/.youtube.js
@@ -1,39 +1,26 @@
-const axios = require('axios');
+const { search: ytSearch, ytmp3 } = require('@vreden/youtube_scraper');
+const yts = require('yt-search');
 
 async function search(name) {
     try {
-        const searchRes = await axios.get(`https://api.paxsenix.biz.id/yt/search?q=${encodeURIComponent(name)}`);
-        if (!searchRes.data.results?.length) return { ok: false, msg: 'Não encontrei nenhuma música.' };
-        return { ok: true, criador: 'Hiudy', data: searchRes.data.results[0] };
+        const searchRes = await yts(name);
+        if (!searchRes.videos || searchRes.videos.length === 0) {
+            return { ok: false, msg: 'Não encontrei nenhuma música.' };
+        };
+        return { ok: true, criador: 'Hiudy', data: searchRes.videos[0] };
     } catch (e) {
         return { ok: false, msg: 'Ocorreu um erro ao realizar a pesquisa.' };
-    }
-}
+    };
+};
 
 async function mp3(url) {
-    try {/*
-        const downloadRes = await axios.get(`https://p.oceansaver.in/ajax/download.php?api=dfcb6d76f2f6a9894gjkege8a4ab232222&url=${encodeURIComponent(url)}&format=m4a`);
-        if (!downloadRes.data.id) return { ok: false, msg: 'Erro ao iniciar o download.' };
-        
-        let attempts = 0;
-        while (attempts < 35) {
-            try {
-                await new Promise(res => setTimeout(res, 3000));
-                const taskStatus = await axios.get(`https://p.oceansaver.in/ajax/progress.php?id=${downloadRes.data.id}`);
-                if (taskStatus.data.success === 1) {
-                    return { ok: true, criador: 'Hiudy', url: taskStatus.data.download_url };
-                }
-                attempts++;
-            } catch (e) {}
-        }
-   
-        return { ok: false, msg: 'O download demorou muito e foi cancelado.' };
-        */
-        datinha = await axios.get(`https://nayan-video-downloader.vercel.app/ytdown?url=${encodeURIComponent(url)}`)
-        return { ok: true, criador: 'Hiudy', url: datinha.data.data.audio };
+    try {
+        const downloadRes = await ytmp3(url);
+        if (!downloadRes) return { ok: false, msg: 'Erro ao iniciar o download.' };
+        return { ok: true, criador: 'Hiudy', url: downloadRes.download.url };
     } catch (e) {
         return { ok: false, msg: 'Ocorreu um erro ao realizar o download.' };
-    }
-}
+    };
+};
 
 module.exports = { search, mp3 };
