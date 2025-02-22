f4834e6770837e2be842fa539439c40f31b0d972
hiudyy <hiudyyy@gmail.com>
2025-02-22 13:30:14 -0300

    atualizando...

--- dados/src/menus/menudown.js
+++ dados/src/menus/menudown.js
@@ -8,17 +8,9 @@ async function menudown(prefix) {
 │  🛠️ *Comandos disponíveis* 
 ├─────────────────────┤
 │🎥 *${prefix}tiktok* ou *${prefix}ttk*
-│  ➥ _Baixa vídeos e fotos do TikTok._
-│  ➥ _Uso: ${prefix}tiktok <link ou nome>_
 │📌 *${prefix}pinterest* ou *${prefix}pin*
-│  ➥ _Baixa fotos e vídeos do Pinterest._
-│  ➥ _Uso: ${prefix}pinterest <link ou nome>_
 │🎶 *${prefix}play*
-│  ➥ _Baixa músicas pelo nome._
-│  ➥ _Uso: ${prefix}play <nome da música>_
 │💫 *${prefix}instagram* ou *${prefix}ig*
-│  ➥ _Baixa fotos e vídeos do Instagram._
-│  ➥ Uso: ${prefix}instagram <link>
 ╰─────────────────────╯
 
 ╭━━━━━━━━━━━━━━━━━━━━━╮
