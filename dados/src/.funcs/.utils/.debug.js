dfb221d96f96b035f66d42370bec7a78bf06c63b
hiudyy <hiudyyy@gmail.com>
2025-02-26 15:13:52 -0300

    atualizando...

--- dados/src/.funcs/.utils/.debug.js
+++ dados/src/.funcs/.utils/.debug.js
@@ -3,7 +3,7 @@
 */
 //Criador: hiudy
 //Versão: 0.0.1
-//Esse arquivo contem direitos autorais, caso meus creditos sejam tirados poderei tomar medidas jurídicas.
+//Esse arquivo contem direitos autorais, caso meus creditos sejam tirados poderei tomar medidas jurídicas
 
 const axios = require('axios');
 
