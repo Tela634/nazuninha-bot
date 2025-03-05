5859199c1a3f244058d3f80afea24517e08b679b
hiudyy <hiudyyy@gmail.com>
2025-03-05 00:37:57 -0300

    atualizando...

--- dados/src/panel/server.js
+++ dados/src/panel/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = 2012;
 
 // Set EJS as templating engine
 app.set('view engine', 'ejs');
