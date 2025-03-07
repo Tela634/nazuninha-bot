4662368ef27b2bf449101fcc4dbdc15ff11bfca3
hiudyy <hiudyyy@gmail.com>
2025-03-07 12:05:10 -0300

    atualizando...

--- dados/src/funcs/utils/debug.js
+++ dados/src/funcs/utils/debug.js
@@ -2,6 +2,6 @@
  *****NÃO MEXA EM NADA AQUI*****
 */
 
-async function reportError(error, version) {console.log('❌ Sistema de debug retirado por motivo de flood'};
+async function reportError(error, version) {console.log('❌ Sistema de debug retirado por motivo de flood')};
 
 module.exports = reportError;
