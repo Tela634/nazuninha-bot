5c166c17099687963405bea6e70146eb7723c260
hiudyy <hiudyyy@gmail.com>
2025-04-10 13:23:12 -0300

    Updating...

--- dados/src/index.js
+++ dados/src/index.js
@@ -1609,7 +1609,7 @@ break;
   
   
   //RPG
-  case 'pix':case 'reg':case 'registrar':case 'delrg':case 'saldo':case 'banco':case 'depositar':case 'deposito':case 'sacar':case 'saque':case 'depoall':case 'saqueall':case 'trabalhar':case 'empregos':case 'trabalhos':case 'addemprego':case 'demissao':case 'delemprego':case 'loja':case 'comprar':case 'vender':case 'me':case 'inventario':case 'pescar':case 'minerar':case 'mina':case 'cacar':case 'assaltar':case 'namorar':case 'aceitar':case 'recusar':case 'divorcio':case 'casar':case 'minhadupla':case 'adotar':case 'soltar':case 'alimentar':case 'banho':case 'tosar':case 'veterinario':case 'meupet':case 'cassino':case 'apostar':case 'brincar':case 'passear':case 'carinho':
+  case 'pix':case 'reg':case 'registrar':case 'delrg':case 'saldo':case 'banco':case 'depositar':case 'deposito':case 'sacar':case 'saque':case 'depoall':case 'saqueall':case 'trabalhar':case 'empregos':case 'trabalhos':case 'addemprego':case 'demissao':case 'delemprego':case 'loja':case 'comprar':case 'vender':case 'me':case 'inventario':case 'pescar':case 'minerar':case 'mina':case 'cacar':case 'assaltar':case 'namorar':case 'aceitar':case 'recusar':case 'divorcio':case 'casar':case 'minhadupla':case 'cassino':case 'apostar':case 'pet':
     try {
         if (!isGroup) return reply('Este comando só pode ser utilizado em grupos.');
         if (!isModoRpg) return reply('Este comando só pode ser utilizado com o modo RPG ativo.');
